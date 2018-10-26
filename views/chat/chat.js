0d33f0b30293f237d43f28d8ec08c2b1e52c4157
JmsMaupin1 <jmsMaupin1@yahoo.com>
2018-10-25 23:09:10 -0400

    Finish initial mockup of chat UI

--- views/chat/chat.js
+++ views/chat/chat.js
@@ -1,12 +1,12 @@
 const socket = io.connect('http://localhost:8080');
 
 // Query DOM
-const message = document.getElementById('message'),
-	send = document.getElementById('send'),
-	clear = document.getElementById('clear'),
-	output = document.getElementById('output'),
-	feedback = document.getElementById('feedback'),
-	chatwindow = document.getElementById('chat-window');
+// const message = document.getElementById('message'),
+// 	send = document.getElementById('send'),
+// 	clear = document.getElementById('clear'),
+// 	output = document.getElementById('output'),
+// 	feedback = document.getElementById('feedback'),
+// 	chatwindow = document.getElementById('chat-window');
 
 
 let handle;
@@ -21,31 +21,31 @@ $.ajax({
 });
 
 // Event Listeners
-send.addEventListener('click', () => {
-	socket.emit('chat', {
-		message: message.value,
-		handle: handle
-	})
+// send.addEventListener('click', () => {
+// 	socket.emit('chat', {
+// 		message: message.value,
+// 		handle: handle
+// 	})
 
-	message.value = '';
-})
+// 	message.value = '';
+// })
 
-message.addEventListener('keydown', (event) => {
-	if(event.which === 13) {
-		socket.emit('chat', {
-			message: message.value,
-			handle: handle
-		})
+// message.addEventListener('keydown', (event) => {
+// 	if(event.which === 13) {
+// 		socket.emit('chat', {
+// 			message: message.value,
+// 			handle: handle
+// 		})
 
-		message.value = '';
-	}
-})
+// 		message.value = '';
+// 	}
+// })
 
-message.addEventListener('keypress', () => {
-	socket.emit('typing', {
-		handle: handle
-	})
-});
+// message.addEventListener('keypress', () => {
+// 	socket.emit('typing', {
+// 		handle: handle
+// 	})
+// });
 
 // Listen for socket events
 socket.on('chat', data => {
